a81ac48fa691eff7f6e3df4dc6e4998959da8b78
Frederic Ducrot <fducrot@c1r3s4.42lausanne.ch>
2025-12-27 10:26:18 +0100

    save

--- srcs/main.c
+++ srcs/main.c
@@ -3,5 +3,16 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    
+	char	*line;
+
+	while (1)
+	{
+		line = readline("Minishell > ");
+		if(!line)
+			ft_printf("Exit\n");
+		if(line)
+			ft_printf("%s", line);
+		free(line);
+	}
+	return (0);
 }
