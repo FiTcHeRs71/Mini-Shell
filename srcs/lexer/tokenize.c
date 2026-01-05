ac2e989e5be04ef7873bb617b80f65e279ed9990
leogranger <leograngerr@gmail.com>
2026-01-05 16:22:30 +0100

    lex almost done

--- srcs/lexer/tokenize.c
+++ srcs/lexer/tokenize.c
@@ -2,11 +2,8 @@
 
 void	tokenize(t_token **token, t_token *new, char *buffer)
 {
-	int i;
-
-	i = 0;
 	new->value = ft_strdup(buffer);
-	if (ft_isalnum(buffer[i]))
+	if (ft_isalnum(*buffer))
 		new->type = TOKEN_WORD;
 	else if (!ft_strncmp(buffer, "|", 2))
 		new->type = TOKEN_PIPE;
@@ -22,6 +19,10 @@ void	tokenize(t_token **token, t_token *new, char *buffer)
 		new->type = TOKEN_AND;
 	else if (!ft_strncmp(buffer, "||", 3))
 		new->type = TOKEN_OR;
+	else if (!ft_strncmp(buffer, "(", 2))
+		new->type = TOKEN_LPAREN;
+	else if (!ft_strncmp(buffer, ")", 2))
+		new->type = TOKEN_RPAREN;
 	// else
 	// 	ft_error();
 	add_back_token(token, new);
@@ -38,9 +39,9 @@ void tokenisation(t_token **token, char *line)
 	{
 		new_tok = new_token();
 		while (*line == ' ' && *line)
-		{
 			line++;
-		}
+		if (!*line)
+			break ;
 		if (*line == '\'')
 		{
 			new_tok->quote = SINGLE_QUOTE;
@@ -51,13 +52,23 @@ void tokenisation(t_token **token, char *line)
 			new_tok->quote = DOUBLE_QUOTE;
 			line++;
 		}
-		else 
-		    new_tok->quote = NO_QUOTE;
+		else
+			new_tok->quote = NO_QUOTE;
 		buf_i = 0;
 		if (new_tok->quote != NO_QUOTE)
 		{
-			while (*line != '\'' || *line != '"')
+			while (*line != ' ' && *line)
 			{
+				if (*line == '"')
+				{
+					line++;
+					break ;
+				}
+				if (*line == '\'')
+				{
+					line++;
+					break ;
+				}
 				c = *line;
 				buffer[buf_i++] = c;
 				line++;
@@ -65,10 +76,19 @@ void tokenisation(t_token **token, char *line)
 			buffer[buf_i] = '\0';
 			tokenize(token, new_tok, buffer);
 		}
-		else
+		else if (*line == '(')
+		{
+			buffer[buf_i++] = '(';
+			buffer[buf_i] = '\0';
+			tokenize(token, new_tok, buffer);
+			line++;
+		}
+		else if (new_tok->quote == NO_QUOTE && *line != ')')
 		{
 			while (*line != ' ' && *line)
 			{
+				if (*line == ')')
+					break ;
 				c = *line;
 				buffer[buf_i++] = c;
 				line++;
@@ -76,6 +96,12 @@ void tokenisation(t_token **token, char *line)
 			buffer[buf_i] = '\0';
 			tokenize(token, new_tok, buffer);
 		}
-		line++;
+		else if (*line == ')')
+		{
+			buffer[buf_i++] = ')';
+			buffer[buf_i] = '\0';
+			tokenize(token, new_tok, buffer);
+			line++;
+		}
 	}
 }
--- srcs/main.c
+++ srcs/main.c
@@ -9,11 +9,11 @@ int main(int argc, char **argv, char **envp)
 	t_shell	shell;
 	t_token	*token_list;
 
-	token_list = NULL;
 	ft_memset(&shell, 0, sizeof(shell));
 	init_shell(&shell, envp);
 	while (true)
 	{
+		token_list = NULL;
 		line = readline("Minishell > ");
 		if(!line) // crtl - D
 		{
@@ -27,12 +27,10 @@ int main(int argc, char **argv, char **envp)
 			tokenisation(&token_list, line);
 			while (token_list)
 			{
-				printf("new token value is : %s\n", token_list->value);
+				printf("new token value is : %s, %d, %d\n", token_list->value, token_list->type, token_list->quote);
 				token_list = token_list->next;
 			}
-			ft_printf("check\n");
 			parse(token_list);
-			ft_printf("check\n");
 		}
 	}
 	rl_clear_history();
