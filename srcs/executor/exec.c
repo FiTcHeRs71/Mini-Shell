0e6101866b3c5df8285803d50227981cd0880292
leogranger <leograngerr@gmail.com>
2026-01-07 17:01:16 +0100

    exec + subshell

--- srcs/executor/exec.c
+++ srcs/executor/exec.c
@@ -17,8 +17,33 @@ int	exec_or(t_shell *shell, t_ast_node *node)
 
 	signal = exec_ast(shell, node->left);
 	if (signal != 0)
-		return (exec_ast(shell, node));
+		return (exec_ast(shell, node->right));
 	return (signal);
+}
+
+int	exec_sub(t_shell *shell, t_ast_node *node)
+{
+	int	pid;
+	int	status;
+	int	code;
+
+	if (shell->is_child)
+		return (exec_ast(shell, node->left));
+	pid = fork();
+	// if (pid < 0)
+	// 	ft_error(); TODO : fork error
+	if (pid == 0)
+	{
+		code = exec_ast(shell, node->left);
+		exit(code);
+	}
+	if (waitpid(pid, &status, 0) < 0)
+		return (1);
+	if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	else if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	return (1);
 }
 
 int	exec_ast(t_shell *shell, t_ast_node *node)
@@ -36,7 +61,7 @@ int	exec_ast(t_shell *shell, t_ast_node *node)
 	else if (node->type == NODE_AND)
 		return (exec_and(shell, node));
 	else if (node->type == NODE_SUBSHELL)
-		return (exec_sub());
+		return (exec_sub(shell, node));
 }
 
 // exec(node):
