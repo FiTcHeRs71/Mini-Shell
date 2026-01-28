425d88f555aa14fe38e63a862dcb7d481c91ec60
Frederic Ducrot <fducrot@c1r3s4.42lausanne.ch>
2026-01-28 18:04:32 +0100

    save

--- srcs/signal/signal.c
+++ srcs/signal/signal.c
@@ -2,17 +2,12 @@
 
 void	handle_sigint(int sig)
 {
-	char *line;
-
 	(void)sig;
 	g_signal = 130;
 	write(1, "\n", 1);
 	rl_on_new_line();
 	rl_replace_line("", 0);
-	line = get_next_line(0);
-	if (!line)
-		rl_redisplay();
-	free(line);
+	rl_redisplay();
 }
 
 void	init_signal(void)
